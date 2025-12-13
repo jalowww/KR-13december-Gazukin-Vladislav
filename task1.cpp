4a0602889d07ab8fcf35366a3a39c0b1026aafe0
jalowww <vladik.gazukin@gmail.com>
2025-12-13 10:26:06 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 15, b = 4;
+    cout << a + b << " ";
+    cout << a - b << " ";
+    cout << a * b << " ";
+    cout << a / b << " ";
+    cout << a % b << " ";
+    return 0; 
+
+}
